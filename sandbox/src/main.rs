9cd729e7afe4c7aa95428de867fc4ed1679c7ba9
RyutaGoto <kasa8gasa2rs6@gmail.com>
2024-09-08 23:57:29 +0900

    add

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,29 +1,9 @@
-use std::io;
-
 fn main() {
-    let a = [1, 2, 3, 4, 5];
+    println!("Hello, world!");
 
-    println!("Please enter an array index.");
-           // 配列の何番目の要素にアクセスするか指定してください
+    another_function(5, 'h');
+}
 
-    let mut index = String::new();
-
-    io::stdin()
-        .read_line(&mut index)
-        .expect("Failed to read line");
-        // 値の読み込みに失敗しました
-
-    let index: usize = index
-        .trim()
-        .parse()
-        .expect("Index entered was not a number");
-        // 入力された値は数字ではありません
-
-    let element = a[index];
-
-    println!(
-        "The value of the element at index {} is: {}",
-        // {}番目の要素の値は{}です
-        index, element
-    );
+fn another_function(value: i32, unit_label:char) {
+    println!("The measurement is: {}{}", value, unit_label);
 }
